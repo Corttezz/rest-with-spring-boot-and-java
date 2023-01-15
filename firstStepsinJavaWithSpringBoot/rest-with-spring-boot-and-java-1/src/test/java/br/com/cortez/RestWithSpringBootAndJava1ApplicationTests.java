29eed8c359408e8c17c30ac6248cc3daa49035fd
Daniel Cortez <106662629+Corttezz@users.noreply.github.com>
2023-01-14 23:15:38 -0300

    First Steps in Java with Spring Boot

---
+++ firstStepsinJavaWithSpringBoot/rest-with-spring-boot-and-java-1/src/test/java/br/com/cortez/RestWithSpringBootAndJava1ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cortez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithSpringBootAndJava1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
